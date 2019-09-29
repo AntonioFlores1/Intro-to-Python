e025a3c37066c3bfaf406121a67b0e1c950f79a6
AntonioFlores1 <af513249@gmail.com>
2019-09-29 17:29:13 -0400

    Going over fundamentals of Python

---
+++ Python.py
@@ -0,0 +1,53 @@
+
+print("hello world")
+
+#Mathmatical Operations
+x = (10+40)/10
+x += 10
+x -= 10
+print(x)
+
+floorDivision = 9//2
+modules = 9%3
+expon = 3**3
+
+print('Floor Division = {floorDivision},Modules = {modules}, Exponents = {expon}'.format(floorDivision = floorDivision,modules = modules,expon = expon))
+
+
+#Data Types
+complex()
+int()
+str()
+float()
+
+#Strings
+# Strings are iterable
+my_name = "Antonio Flores"
+fifth_letter = my_name[5]
+second_to_last = my_name[-2]
+my_name_list = list(my_name)
+
+print(my_name_list)
+print(''.join(my_name))
+
+print(fifth_letter)
+print(second_to_last)
+print(my_name.split(" "))
+print(my_name.upper())
+
+looking_for_firstname = 
+#f-String
+#first = 'Hello'
+#second = f' {first} World'
+#print(second)
+#print(f'Floor Division = {floorDivision}, Modules = {modules}, Exponents = {expon}')
+
+
+#Tuples
+tuple = ('one',2,"three")
+print(tuple)
+
+one,two,three = 1,2,3
+print(one,two,three)
+
+
